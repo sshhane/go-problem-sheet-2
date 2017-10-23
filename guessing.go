6320ec3ce04de37ca333f1cb408617356792009e
SHANE DANIELS - Student <G00332592@gmit.ie>
2017-10-23 21:04:18 +0100

    guessing game print

--- guessing.go
+++ guessing.go
@@ -5,21 +5,12 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"bytes"
 )
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "r.URL:              ",  r.URL              )
-	
+	// fmt.Fprintln(w, "r.URL:              ",  r.URL              )
+	fmt.Fprintln(w, "Guessing Game")
 
-	fmt.Fprintln(w, "Header:")
-	for key, value := range r.Header {
-		fmt.Fprintln(w, "\t" + key + ":", value)
-	}
-
-	body := new(bytes.Buffer)
-	body.ReadFrom(r.Body)
-	fmt.Fprintln(w, "r.Body:             ",  body.String())
 }
 
 func main() {
